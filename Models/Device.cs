2bf1c68fe3ea29d17735f1196f4ae91f3f845107
Bryan Mayer <mayerbryan@mail.com>
2023-11-17 15:13:33 +0000

    device model update

--- Models/Device.cs
+++ Models/Device.cs
@@ -7,8 +7,24 @@ namespace ITMappingSystem.Models
 {
     public class Device
     {
-        public int DeviceID { get; set; }
-        public string? DeviceName { get; set; }
-        
+        public int ID { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string OperatingSystem { get; set; }
+        public int IP { get; set; }
+        public string NetworkPort { get; set; }
+        public string NetworkSwitch { get; set; }
+        public string Owner { get; set; }
+        public string Vendor { get; set; }
+        public string LocationDescription { get; set; }
+        public byte[] LocationMapImage { get; set; }
+        public byte[] Picture { get; set; }
+        public string MappedDrivers { get; set; }
+        public string ConnectedServers { get; set; }
+        public string SoftwareList { get; set; }
+        public DateTime ImplementationDate { get; set; }
+        public DateTime lifeCycle { get; set; }
+        public string RelatedKbs { get; set; }
+      
     }
 }
